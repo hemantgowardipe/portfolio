588231ccc1fb9da75ec02038a6ca583dca5814a3
Hemant <rajugowardipe0@gmail.com>
2025-08-24 12:10:46 +0530

    update the css entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './styles.css';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
